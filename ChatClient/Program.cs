06b7945484b72817d235c90b395f7f731bfe862c
fedosovmu <fedosovmu@gmail.com>
2017-06-07 02:10:52 +0500

    Улучьшаем этот говнокод

--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -14,7 +14,7 @@ namespace ChatClient
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new EnterWindow());
+            Application.Run(new ChatForm());
         }
     }
 }
